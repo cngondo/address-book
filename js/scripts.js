fb1061dfa9615f3d7248c1990ced6c1e4dd9cc70
Epicodus <mike@epicodus.com>
2016-03-03 15:23:16 -0800

    add address constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
