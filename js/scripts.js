a88421157684bb5f0c4c232ac2acddbf5b1eb205
Epicodus <mike@epicodus.com>
2016-03-03 12:32:03 -0800

    add contact full info display when contact is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,13 @@ $(document).ready(function() {
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
   });
