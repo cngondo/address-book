8811f7c6184725d5a98a73f2bd8b2a2592e3874d
Epicodus <mike@epicodus.com>
2016-03-03 16:03:24 -0800

    add jquery to show more address fields when another address button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,24 @@ Contact.prototype.fullName = function() {
 
 // user interface logic
 $(document).ready(function() {
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
