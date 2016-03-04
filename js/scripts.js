6ada0e651b1605c01167613f02d0d5fac6dfe1bc
Epicodus <mike@epicodus.com>
2016-03-03 16:04:33 -0800

    collect address data from form and turn into address objects

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,14 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
